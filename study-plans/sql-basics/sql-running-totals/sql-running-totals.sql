8cf2a8896a68264d9c53348609f540a1c7531d44
Madan Raj <53041080+maximus0x0@users.noreply.github.com>
2026-04-14 03:27:25 +0530

    Update solution for sql-running-totals

--- study-plans/sql-basics/sql-running-totals/sql-running-totals.sql
+++ study-plans/sql-basics/sql-running-totals/sql-running-totals.sql
@@ -3,6 +3,7 @@ select
     account,
     txn_date,
     amount,
-    sum(amount) over(partition by account order by txn_date) as running_total
+    sum(amount) over(partition by account order by txn_date,id
+    ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as running_total
 from transactions
 order by account asc, txn_date asc, id asc
