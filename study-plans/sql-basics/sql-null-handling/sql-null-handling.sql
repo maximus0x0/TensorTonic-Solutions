90bedba1680cb6eb8d088281a2bf012831a4a61e
Madan Raj <53041080+maximus0x0@users.noreply.github.com>
2026-04-14 03:35:48 +0530

    Update solution for sql-null-handling

--- study-plans/sql-basics/sql-null-handling/sql-null-handling.sql
+++ study-plans/sql-basics/sql-null-handling/sql-null-handling.sql
@@ -1,8 +1,15 @@
--- Write your SQL query here
-select 
-    name,
-    (case when email IS NOT NULL then email else 'N/A' end) as display_email,
-    (case when deactivated_at IS NULL then 'active' else 'inactive' end) as status
-    from customers
-    WHERE phone IS NOT NULL
-order by name asc
+-- -- Write your SQL query here
+-- select 
+--     name,
+--     (case when email IS NOT NULL then email else 'N/A' end) as display_email,
+--     (case when deactivated_at IS NULL then 'active' else 'inactive' end) as status
+--     from customers
+--     WHERE phone IS NOT NULL
+-- order by name asc
+
+SELECT name,
+       COALESCE(email, 'N/A') AS display_email,
+       CASE WHEN deactivated_at IS NULL THEN 'active' ELSE 'inactive' END AS status
+FROM customers
+WHERE phone IS NOT NULL
+ORDER BY name ASC;
